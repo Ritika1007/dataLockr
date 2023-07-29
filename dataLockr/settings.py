95caed0bbd306caaf2813abf5856f46baad58236
Ritika Arora <ritika10arora@gmail.com>
2023-07-29 14:27:26 +0530

    Settings and URLs for API

--- dataLockr/settings.py
+++ dataLockr/settings.py
@@ -42,9 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework',
 
     #myapp
     'dataApp',
+    'dataAPI',
 ]
 
 MIDDLEWARE = [
