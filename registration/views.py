beeaa3dfff3a5a86b1a69875237b9a6a9d786a5b
root <root@DESKTOP-RITIKA.localdomain>
2023-08-26 17:36:46 +0530

    Added sweet alerts

--- registration/views.py
+++ registration/views.py
@@ -3,6 +3,7 @@ from .forms import *
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages
+import sweetify
 
 def register_request(request):
     if request.method == "POST":
@@ -10,9 +11,11 @@ def register_request(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            messages.success(request, "Registration Successful.")
+            # messages.success(request, "Registration Successful.")
+            sweetify.success(request, 'Registration Successful.')
             return redirect('/')
-        messages.error(request, "Unsuccessful registration. Invalid information.")
+        # messages.error(request, "Unsuccessful registration. Invalid information.")
+        sweetify.error(request, 'Unsuccessful registration. Invalid information.')
     form = NewUserForm()
     return render (request=request, template_name="registration/register.html", context={"register_form":form})
 
@@ -26,17 +29,21 @@ def login_request(request):
             user = authenticate(username=username, password = password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f"You are now logged in as {username}.")
+                # messages.info(request, f"You are now logged in as {username}.")
+                sweetify.success(request, 'Logged in Successfully!')
                 return redirect("/")
             else:
-                messages.error(request,"Invalid username or password.")
+                # messages.error(request,"Invalid username or password.")
+                sweetify.error(request, 'Invalid login credentials.')
         else:
-            messages.error(request,"Invalid username or password.")
+            # messages.error(request,"Invalid username or password.")
+            sweetify.error(request, 'Invalid login credentials.')
     
     form = AuthenticationForm()
     return render (request=request, template_name="registration/login.html", context={"login_form":form})
 
 def logout_request(request):
     logout(request)
-    messages.info(request, "Logged out successfully!")
+    # messages.info(request, "Logged out successfully!")
+    sweetify.success(request, 'Logged Out Successfully!')
     return redirect('/login')
