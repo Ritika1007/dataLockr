bf086a8bf50f6769c4e94628a28cdcdabee69e74
Ritika Arora <ritika10arora@gmail.com>
2023-09-05 23:05:43 +0530

    Get Folder's List for authenticated user

--- data_lockr_api.py
+++ data_lockr_api.py
@@ -15,22 +15,37 @@ BASE_URL = "http://127.0.0.1:8000"
 
 def get_token(params):
     """function to get user jwt token"""
+    end_point = '/api/token/'
     get_jwt_token = requests.post(
-        f'{BASE_URL}/api/token/',  params)
+        f'{BASE_URL}{end_point}',  params)
 
     result = json.loads(get_jwt_token.text)
     try:
         return result['access']
     except KeyError:
-        cprint('\nInvaid User!','red')
+        cprint('\nInvaid User!', 'red')
 
 
-#Get User Creds
+def get_folders_list(token):
+    """get lits of folders created by user"""
+    end_point = '/v1/api/subfolders/'
+    headers = {
+        'Authorization': f'Bearer {token}',
+        'Content-Type': 'application/json'
+    }
+    result = requests.get(
+        f'{BASE_URL}{end_point}', headers=headers
+    )
+    cprint(json.dumps(json.loads(result.text), indent=4, sort_keys=True), 'green')
+
+
+# Get User Creds
 username = input('Enter Username: ')
 password = getpass.getpass(prompt='Enter Passowrd: ')
 
-user_details={
+user_details = {
     'username': username,
-    'password': password 
-        }
-get_token(user_details)
+    'password': password
+}
+access_token = get_token(user_details)
+get_folders_list(access_token)
