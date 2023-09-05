41765cad02f9362d0bfcf95d83b8094231111b73
Ritika Arora <ritika10arora@gmail.com>
2023-09-05 22:49:58 +0530

    updates for user unput and exception handling

--- data_lockr_api.py
+++ data_lockr_api.py
@@ -5,21 +5,32 @@ script to test dataLockr APIs using Python3 requests.
 """
 
 import json
+import getpass
+from termcolor import cprint
 import requests
+
 
 BASE_URL = "http://127.0.0.1:8000"
 
 
 def get_token(params):
-    """function to get user token"""
+    """function to get user jwt token"""
     get_jwt_token = requests.post(
         f'{BASE_URL}/api/token/',  params)
 
     result = json.loads(get_jwt_token.text)
-    print(result['access'])
+    try:
+        return result['access']
+    except KeyError:
+        cprint('\nInvaid User!','red')
+
+
+#Get User Creds
+username = input('Enter Username: ')
+password = getpass.getpass(prompt='Enter Passowrd: ')
 
 user_details={
-    'username': 'ritika.1',
-    'password': 'Confirm@121' 
+    'username': username,
+    'password': password 
         }
 get_token(user_details)
